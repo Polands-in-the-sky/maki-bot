e73732802180fca434815873ab9be172a3cc1c23
Polands in the Sky <glugglugglug@vivaldi.net>
2018-05-27 08:52:07 +0800

    removing rubbish

--- bot.py
+++ bot.py
@@ -6,10 +6,11 @@ import asyncio
 import random
 from discord import Client
 import redis
+import urbandictionary as ud
 r = redis.Redis(host='localhost', port=6379, db=0)
 bot = commands.Bot(command_prefix='$')
 dadjokes=["Can february march? No, but april may","I won't buy anything velcro. They are such a rip-off"]
-print('Hello')
+
 @bot.event
 async def on_ready():
     print ("Maki is here.")
